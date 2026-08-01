fb594326df654e0715cd5ce6a2a50ae347577e6e
kaybee09 <kaybeelateef@gmail.com>
2026-08-01 21:24:00 +0200

    LVM

---
+++ scripts/alma-snapshot-dr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+usage=$(lvs --noheadings -o data_percent corp_storage/thinpool | tr -d ' %')
+
+usage=${usage%.*}
+
+if [ "$usage" -ge 90 ]; then
+    logger "CRITICAL: Thin Pool usage is ${usage}%"
+elif [ "$usage" -ge 80 ]; then
+    logger "WARNING: Thin Pool usage is ${usage}%"
+fi
